1f1d4090dfba99d4c7b3a53575c0e0e3e241f16e
Luiz Gabriel Pessanha <140475020+LuizDev271@users.noreply.github.com>
2024-07-31 20:33:49 -0300

    HTML5 - / JS + (dnv)

---
+++ leis.js
@@ -0,0 +1,25 @@
+let imagemPrincipal = document.getElementById('imagem-principal')
+
+function anteProjeto(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Lei-6.jpg'
+}
+
+function n9249(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Lei-2.jpg'
+}
+
+function n0265(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Lei-1.jpg'
+}
+
+function n9423(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Lei-3.jpg'
+}
+
+function n9386(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Lei-4.jpg'
+}
+
+function maisLeis(){
+    imagemPrincipal.src = '../imagens/Maicon/leis/Algumas_Leis.jpg'
+}
